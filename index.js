9565d35f6614f92e2f7933703f7e71e9dea988e7
sotatek-truongpham <truongpham@sotatek.com>
2020-10-12 09:27:59 +0700

    add socket emit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var socketIndex = require.main.require('./src/socket.io');
 var EmitSocket = {
         emitPostSave: function(postData) {
             console.log(postData);
+            socketIndex.server.sockets.emit('hello-wold', postData);
+            console.log('abc');
         }
     };
 
